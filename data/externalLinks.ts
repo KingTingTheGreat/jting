04b661565403cc3d5a4b0983d42cc0ca23826288
Jeffrey <jting@bu.edu>
2024-01-25 15:37:24 -0500

    corrected resume path

--- data/externalLinks.ts
+++ data/externalLinks.ts
@@ -1,18 +1,18 @@
 import { LinkType } from "@/types";
 
 const externalLinks: LinkType[] = [
-	// {
-	// 	name: "GitHub",
-	// 	url: "https://github.com/kingtingthegreat/",
-	// 	image: "/github-mark-white.png",
-	// },
+	{
+		name: "GitHub",
+		url: "https://github.com/kingtingthegreat/",
+		// image: "/github-mark-white.png",
+	},
 	{
 		name: "LinkedIn",
 		url: "https://www.linkedin.com/in/jeffrey-ting-08b606253/",
 	},
 	{
 		name: "Resume",
-		url: "public/TingJeffrey-Resume.pdf",
+		url: "/TingJeffrey-Resume.pdf",
 		type: "application/pdf",
 	},
 ];
