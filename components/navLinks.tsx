e5ae6af543cd6fb3ee55aa9d9c71e68904afeb8e
Jeffrey <jting@bu.edu>
2024-01-30 12:40:30 -0500

    capitalize links with css

--- components/navLinks.tsx
+++ components/navLinks.tsx
@@ -5,8 +5,8 @@ const StringLink = ({ link }: { link: string }) => {
 	// ensure the link is lowercase
 	const lower = link.toLowerCase();
 	return (
-		<Link href={`#${lower}`} className="p-1 m-1 bg-black flex-1 text-center">
-			{link.charAt(0).toUpperCase() + link.slice(1)}
+		<Link href={`#${lower}`} className="p-1 m-1 bg-black flex-1 text-center capitalize">
+			{link}
 		</Link>
 	);
 };
