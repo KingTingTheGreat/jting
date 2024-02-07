5eb5a2a29a1e1961b91aa78f254a08c6dddce717
KingTingTheGreat <jting@bu.edu>
2024-02-07 11:15:52 -0500

    corrected id so internal links work

--- components/sectionWrapper.tsx
+++ components/sectionWrapper.tsx
@@ -1,6 +1,6 @@
 const SectionWrapper = ({ title, children }: { title: string; children: React.ReactNode }) => {
 	return (
-		<div id="title" className="flex flex-col justify-center m-4 w-full">
+		<div id={title} className="flex flex-col justify-center m-4 w-full">
 			<h1 className="text-5xl font-semibold capitalize">{title}</h1>
 			{children}
 		</div>
