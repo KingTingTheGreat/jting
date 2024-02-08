89c40ab6faaf8ead9e8e7c506d2535e6d24ab79a
KingTingTheGreat <jting@bu.edu>
2024-02-07 21:56:12 -0500

    corrected links to certs

--- components/additionalInfo.tsx
+++ components/additionalInfo.tsx
@@ -5,7 +5,7 @@ import externalLinks from "@/data/externalLinks";
 
 const LinkWrapper = ({ link }: { link: exLink }) => {
 	return (
-		<div className="items-center text-center p-1.5 m-2 bg-black rounded-lg border-2 border-slate-700 border-solid bg-[#62a590] cursor-pointer hover:border-blue-300">
+		<div className="items-center text-center p-1.5 m-2 bg-black rounded-lg border-2 border-slate-700 border-solid cursor-pointer hover:border-blue-300">
 			<Link href={link.url} target="_blank" type={link.type ?? ""}>
 				{link.image ? <Image src={link.image} alt={link.name} fill={true} objectFit="contain" /> : link.name}
 			</Link>
--- data/internalPages.ts
+++ data/internalPages.ts
@@ -170,15 +170,25 @@ const internalPages: internalPagesType = {
 				title: "Foundational C# with Microsoft",
 				organization: "Microsoft",
 				issueDate: "June 2023",
-				link: "https://www.youracclaim.com/badges/3b5f2d5e-8f1f-4e1f-8b6f-7e1e2e2d9e3f",
-				description: "This certification demonstrates foundational knowledge of C#.",
+				link: "https://www.freecodecamp.org/certification/KingTing/foundational-c-sharp-with-microsoft",
+				description:
+					"This certification from freeCodeCamp, in collaboration with Microsoft, demonstrates foundational knowledge of C#.",
 			},
 			{
 				title: "Code in Place 2023",
 				organization: "Stanford University",
 				issueDate: "June 2023",
-				link: "https://codeinplace.stanford.edu/certificates/1b2f1f2e3d4e5f6e7d8c9b0a",
-				description: "This certification demonstrates proficiency in Python and Python education.",
+				link: "https://digitalcredential.stanford.edu/check/F80D74C1D0E91EA8600838CB9EF861933B732E94CF97B88B75CB82E96927BBEBblhwblZDaHVWNkJZL0I3RUZsbFA5a1ZHaGhZcTBWcTNIc2NkRDFtQmhUZHhXVXda",
+				description:
+					"This certification from Stanford University demonstrates proficiency in Python and Python education.",
+			},
+			{
+				title: "CodeSignal General Coding Assessment",
+				organization: "CodeSignal",
+				issueDate: "November 2022",
+				link: "https://app.codesignal.com/coding-report/yDPtEjSLcEY8ojN9E-dpqhapqiHxh9fEiqCzPt23E4?accessToken=yDPtEjSLcEY8ojN9E-oeqwj5xwCqekAyFcEZWrehHy",
+				description:
+					"This certification shows a certified perfect score on the CodeSignal General Coding Assessment (GCA).",
 			},
 		],
 	},
