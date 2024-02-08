c13a3df9f1cc8e1bfec6121a21dc8824d0426606
KingTingTheGreat <jting@bu.edu>
2024-02-08 09:17:17 -0500

    temporary stuff for contact link

--- components/navLinks.tsx
+++ components/navLinks.tsx
@@ -2,6 +2,15 @@ import Link from "next/link";
 import internalPages from "@/data/internalPages";
 
 const StringLink = ({ link }: { link: string }) => {
+	if (link === "contact") {
+		return (
+			<Link
+				href={`mailto: jting@bu.edu`}
+				className="p-1 md:p-2 text-sm md:text-xl capitalize rounded-md m-0.5 md:my-1.5 md:mx-2 transition flex-1 bg-black border-[2px] md:border-[6px] border-solid border-white text-center hover:border-blue-400 hover:text-blue-100">
+				{link}
+			</Link>
+		);
+	}
 	return (
 		<Link
 			href={`#${link.toLowerCase()}`}
